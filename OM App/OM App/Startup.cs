52253dbfd73b0d97335fe52230c17768af86ade2
Mandy Makweni <mandy@puresurvey.co.za>
2018-06-22 14:18:16 +0200

    tasks

---
+++ OM App/OM App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OM_App.Startup))]
+namespace OM_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ others/Projects/OM App/OM App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OM_App.MvcApplication" Language="C#" %>
